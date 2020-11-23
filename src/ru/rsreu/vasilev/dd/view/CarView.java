efdf07243134ed016477c2ae8418b59bf7d6844f
Piller <vilya1997@bk.ru>
2020-11-24 01:09:22 +0300

    Some changes

--- src/ru/rsreu/vasilev/dd/view/CarView.java
+++ src/ru/rsreu/vasilev/dd/view/CarView.java
@@ -1,20 +1,23 @@
 package ru.rsreu.vasilev.dd.view;
 
 import javafx.application.Platform;
-import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Polygon;
 import ru.rsreu.vasilev.dd.model.Car;
 
-public class CarView extends Rectangle implements ObjectListener {
+public class CarView extends Polygon implements ObjectListener {
     public static final int CAR_WIDTH = 10;
     public static final int CAR_HEIGHT = 20;
     private final Pane root;
 
     public CarView(Pane root) {
         this.root = root;
-        setWidth(CAR_WIDTH);
-        setHeight(CAR_HEIGHT);
+        getPoints().addAll(0., 0.,
+                10.0, 0.,
+                10.0, 20.,
+                0., 20.);
+//        setWidth(CAR_WIDTH);
+//        setHeight(CAR_HEIGHT);
         Platform.runLater(() -> root.getChildren().add(this));
     }
 
